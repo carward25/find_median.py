10783177e37f105418a1f05b4312e5c329539562
carward25 <83989145+carward25@users.noreply.github.com>
2021-11-03 14:52:45 -0700

    Update find_median.py

--- find_median.py
+++ find_median.py
@@ -3,13 +3,10 @@
 #description: this program takes as a parameter a list of numbers.
 #The function should return the statistical median of those numbers, which will require it to sort the list
 
-def find_median(lst):
-    lst.sort()
-    if(len(lst) %2 == 1):
-        return lst[len(lst)//2]
+def find_median(numbers):
+    numbers.sort()
+    if len(numbers) % 2 == 1:
+        return numbers[len(numbers) // 2]
     else:
-        return (lst[len(lst) // 2-1] + lst[len(lst) // 2])/2
+        return (numbers[len(numbers) // 2] + numbers[(len(numbers) - 1) // 2]) / 2
 
-#Testing
-print(find_median([4,2,5,1,3]))
-print(find_median([2,5,1,3]))
