417157782a1f3eb4ee96c06b38ab8cf4e4838917
carward25 <83989145+carward25@users.noreply.github.com>
2021-11-03 14:51:48 -0700

    Update find_median.py

--- find_median.py
+++ find_median.py
@@ -1,3 +1,8 @@
+#name: Cassidy Ward
+#date: 11/3/2021
+#description: this program takes as a parameter a list of numbers.
+#The function should return the statistical median of those numbers, which will require it to sort the list
+
 def find_median(lst):
     lst.sort()
     if(len(lst) %2 == 1):
@@ -5,6 +10,6 @@ def find_median(lst):
     else:
         return (lst[len(lst) // 2-1] + lst[len(lst) // 2])/2
 
-# Testing
+#Testing
 print(find_median([4,2,5,1,3]))
 print(find_median([2,5,1,3]))
